20902f6e9b40dded929a96b9f9c150bd094403d9
Anant Bisht <113544464+AnantBisht07@users.noreply.github.com>
2025-03-28 16:48:53 +0530

    Update Comment.js

--- server/models/Comment.js
+++ server/models/Comment.js
@@ -1,11 +1,26 @@
 const mongoose = require("mongoose");
 
 const commentSchema = new mongoose.Schema({
-    blog: { type: mongoose.Schema.Types.ObjectId, ref: "Blog", required: true }, // Reference to the blog
-    user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true }, // Reference to the user
-    text: { type: String, required: true },
-    createdAt: { type: Date, default: Date.now },
-    parentComment: { type: mongoose.Schema.Types.ObjectId, ref: "Comment", default: null }, // For replies
+    blog: 
+    { 
+        type: mongoose.Schema.Types.ObjectId, ref: "Blog", required: true
+    },
+    user: 
+    { 
+        type: mongoose.Schema.Types.ObjectId, ref: "User", required: true 
+    }, 
+    text: 
+    { 
+        type: String, required: true 
+    },
+    createdAt: 
+    { 
+        type: Date, default: Date.now 
+    },
+    parentComment: 
+    { 
+        type: mongoose.Schema.Types.ObjectId, ref: "Comment", default: null 
+    }, 
 });
 
 const Comment = mongoose.model("Comment", commentSchema);
